1507b1dbe2235014eaf213c76280c85fb3034b39
andriwinanda <andriwinanda98@gmail.com>
2023-11-28 15:11:55 +0700

    update

--- connectMongo.js
+++ connectMongo.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', false)
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
-        console.log("Connect to MongoDB successfully")
-    } catch (error) {
-        console.log("Connect failed " + error.message )
-    }
+	try {
+		await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+		console.log("Connect to MongoDB successfully")
+	} catch (error) {
+		console.log("Connect failed " + error.message )
+	}
 }
 
 module.exports = connectDB
--- index.js
+++ index.js
@@ -1,122 +1,35 @@
-const express = require('express')
+const express = require( 'express' )
+const fileUpload = require( 'express-fileupload' )
+const cors = require( 'cors' )
+const routes = require( './router/routes' )
+const bodyParser = require("body-parser");
+
+
 
 const app = express()
 
-require('dotenv').config()
+require( 'dotenv' ).config()
 
-app.use(express.json())
 
-const connectDB = require('./connectMongo')
+app.use( express.json() )
+app.use( cors() )
+
+// use bodyParser middleware on express app
+app.use(bodyParser.urlencoded({ extended:  true }));
+app.use(bodyParser.json());
+
+// This is Express.js middleware to parse and handle the files we upload from our HTML page
+app.use( fileUpload() )
+
+const connectDB = require( './connectMongo' )
 
 connectDB()
 
-const BookModel = require('./models/book.model')
-
-app.get('/api/v1/books', async (req, res) => {
-
-    const { limit = 5, orderBy = 'name', sortBy = 'asc', keyword } = req.query
-    let page = +req.query?.page
-
-    if (!page || page <= 0) page = 1
-
-    const skip = (page - 1) * +limit
-
-    const query = {}
-
-    if (keyword) query.name = { "$regex": keyword, "$options": "i" }
-
-    try {
-        const data = await BookModel.find(query).skip(skip).limit(limit).sort({[orderBy]: sortBy})
-        const totalItems = await BookModel.countDocuments(query)
-        return res.status(200).json({
-            msg: 'Ok',
-            data,
-            totalItems,
-            totalPages: Math.ceil(totalItems / limit),
-            limit: +limit,
-            currentPage: page
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
-
-app.get('/api/v1/books/:id', async (req, res) => {
-    try {
-        const data = await BookModel.findById(req.params.id)
-
-        if (data) {
-            return res.status(200).json({
-                msg: 'Ok',
-                data
-            })
-        }
-
-        return res.status(404).json({
-            msg: 'Not Found',
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
-
-app.post('/api/v1/books', async (req, res) => {
-    try {
-        const { name, author, price, description } = req.body
-        const book = new BookModel({
-            name, author, price, description
-        })
-        const data = await book.save()
-        return res.status(200).json({
-            msg: 'Ok',
-            data
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
-
-app.put('/api/v1/books/:id', async (req, res) => {
-    try {
-        const { name, author, price, description } = req.body
-        const { id } = req.params
-
-        const data = await BookModel.findByIdAndUpdate(id, {
-            name, author, price, description
-        }, { new: true })
-
-        return res.status(200).json({
-            msg: 'Ok',
-            data
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
-
-app.delete('/api/v1/books/:id', async (req, res) => {
-    try {
-        await BookModel.findByIdAndDelete(req.params.id)
-        return res.status(200).json({
-            msg: 'Ok',
-        })
-    } catch (error) {
-        return res.status(500).json({
-            msg: error.message
-        })
-    }
-})
+app.use( '/api', routes )
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT)
-})
+app.listen( PORT, () =>
+{
+  console.log( "Server is running on port " + PORT )
+} )
