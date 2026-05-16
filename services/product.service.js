a8709421a7751c8faa8283c909953ba6eee1ada4
andriwinanda <andriwinanda98@gmail.com>
2026-05-16 19:38:39 +0700

    update hitung api

--- services/product.service.js
+++ services/product.service.js
@@ -1,124 +1,104 @@
-const ProductModel = require( '../models/product.model' )
-const GlassModel = require( '../models/glass.model' )
-const MaterialModel = require( '../models/material.model' )
-const LocationModel = require( '../models/location.model' )
+const ProductModel = require('../models/product.model')
+const GlassModel = require('../models/glass.model')
+const MaterialModel = require('../models/material.model')
+const LocationModel = require('../models/location.model')
 
 
-async function create ( req, res )
-{
-  try
-  {
+async function create(req, res) {
+  try {
     const { type, series, doorLeaves, description, fixGlassTop, fixGlassBottom, material, imageUrl } = req.body
-    const product = new ProductModel( { type, series, doorLeaves, description, fixGlassTop, fixGlassBottom, material, imageUrl } )
+    const product = new ProductModel({ type, series, doorLeaves, description, fixGlassTop, fixGlassBottom, material, imageUrl })
     const data = await product.save()
-    return res.status( 200 ).json( {
+    return res.status(200).json({
       message: 'Ok',
       data
-    } )
-  } catch ( error )
-  {
-    return res.status( 500 ).json( {
+    })
+  } catch (error) {
+    return res.status(500).json({
       message: error.message
-    } )
+    })
   }
 }
 
-async function findAll ( req, res )
-{
+async function findAll(req, res) {
   const { keyword } = req.query
   const query = {}
-  if ( keyword ) query.name = { "$regex": keyword, "$options": "i" }
-  try
-  {
-    const data = await ProductModel.find( query )
-    data.map(el => el.imageUrl = process.env.ASSETS_URL + el.imageUrl )
-    return res.status( 200 ).json( data )
-  } catch ( error )
-  {
-    return res.status( 500 ).json( {
+  if (keyword) query.name = { "$regex": keyword, "$options": "i" }
+  try {
+    const data = await ProductModel.find(query)
+    data.map(el => el.imageUrl = process.env.ASSETS_URL + el.imageUrl)
+    return res.status(200).json(data)
+  } catch (error) {
+    return res.status(500).json({
       message: error.message
-    } )
+    })
   }
 }
-async function findOne ( req, res )
-{
+async function findOne(req, res) {
   const id = req.params.id
-  try
-  {
-    const data = await ProductModel.findById( id )
+  try {
+    const data = await ProductModel.findById(id)
     data.imageUrl = process.env.ASSETS_URL + data.imageUrl
-    if ( data )
-    {
-      return res.status( 200 ).json( data )
+    if (data) {
+      return res.status(200).json(data)
     }
 
-    return res.status( 404 ).json( {
+    return res.status(404).json({
       message: 'Not Found',
-    } )
-  } catch ( error )
-  {
-    return res.status( 500 ).json( {
+    })
+  } catch (error) {
+    return res.status(500).json({
       message: error.message
-    } )
+    })
   }
 }
 
-async function update ( req, res )
-{
+async function update(req, res) {
   const { type, series, doorLeaves, description, fixGlassTop, fixGlassBottom, material, imageUrl } = req.body
   imageUrl.replace(process.env.ASSETS_URL, '')
-  const product = new ProductModel( { type, series, doorLeaves, description, fixGlassTop, fixGlassBottom, material, imageUrl }, { _id : false } )
+  const product = new ProductModel({ type, series, doorLeaves, description, fixGlassTop, fixGlassBottom, material, imageUrl }, { _id: false })
   const { id } = req.params
-  try
-  {
-    const data = await ProductModel.findByIdAndUpdate( id, product )
-    return res.status( 200 ).json( {
+  try {
+    const data = await ProductModel.findByIdAndUpdate(id, product)
+    return res.status(200).json({
       message: 'Ok',
       data
-    } )
-  } catch ( error )
-  {
-    return res.status( 500 ).json( {
+    })
+  } catch (error) {
+    return res.status(500).json({
       message: error.message
-    } )
+    })
   }
 }
 
-async function deleteOne ( req, res )
-{
+async function deleteOne(req, res) {
   const id = req.params.id
-  try
-  {
-    await ProductModel.findByIdAndDelete( id )
-    return res.status( 200 ).json( {
+  try {
+    await ProductModel.findByIdAndDelete(id)
+    return res.status(200).json({
       message: 'Ok',
-    } )
-  } catch ( error )
-  {
-    return res.status( 500 ).json( {
+    })
+  } catch (error) {
+    return res.status(500).json({
       message: error.message
-    } )
+    })
   }
 }
 
-async function hitung ( req, res )
-{
-  try
-  {
+async function hitung(req, res) {
+  try {
     const { productId, locationId, lebar, tinggi, fixGlassTop, fixGlassBottom, selectedGlass } = req.body
 
-    const productDetail = await ProductModel.findById( productId ) || null
-    console.log(productId, productDetail)
-    const location = await LocationModel.findById( locationId ) || null
-    // const glass = await GlassModel.find( {} ) || null
-    const glass = await ProductModel.findById( selectedGlass ) || null
-    const material = await MaterialModel.find( {} ) || null
-
+    const productDetail = await ProductModel.findById(productId) || null
+    const location = await LocationModel.findById(locationId) || null
+    const glass = await GlassModel.findById(selectedGlass) || null
+    const material = await MaterialModel.find({}) || null
+    
     // Convert to object for easy access
     const materialObj = {}
-    material.forEach( m => {
-      materialObj[m.idMaterial] = m
-    } )
+    material.forEach(m => {
+      materialObj[m._id.toString()] = m
+    })
 
     const summary = {
       material: [],
@@ -126,9 +106,8 @@ async function hitung ( req, res )
     }
     let result = null
 
-    productDetail.material.map( el => {
-      if ( el.idMaterial )
-      {
+    productDetail.material.map(el => {
+      if (el.idMaterial) {
         const item = {
           name: '',
           id: null,
@@ -138,27 +117,26 @@ async function hitung ( req, res )
           itemPrice: null,
           totalPrice: null
         }
-        if ( el.idMaterial === 'Kaca' )
-        {
+        if (el.idMaterial === 'Kaca') {
           const glassItem = glass
+          glassItem.price = {}
           item.name = glassItem.name
-          item.itemPrice = glassItem.price[location.location]
+          item.itemPrice = location.idGlassPrice[selectedGlass]
           item.category = 'glass'
         }
-        else
-        {
-          item.itemPrice = materialObj[el.idMaterial].price[location.location]
+        else {
+          item.itemPrice = location.idMaterialPrice[el.idMaterial]
           item.name = materialObj[el.idMaterial].name
         }
-        const formula = el.formula.replace( 'L', Number( lebar ) ).replace( 'T', Number( tinggi ) ).replace( 'A', Number( fixGlassTop ) ).replace( 'B', Number( fixGlassBottom ) )
+        const formula = el.formula.replace('L', Number(lebar)).replace('T', Number(tinggi)).replace('A', Number(fixGlassTop)).replace('B', Number(fixGlassBottom))
         item.rawformula = el.formula
-        item.formula = eval( formula )
+        item.formula = eval(formula)
         item.id = el.idMaterial
         item.totalPrice = item.itemPrice * item.formula
-        summary.material.push( item )
+        summary.material.push(item)
         summary.total += item.totalPrice
       }
-    } )
+    })
     result = {
       summary: summary,
       lebar: lebar,
@@ -168,24 +146,15 @@ async function hitung ( req, res )
       productDetail: productDetail,
       location: location.location
     }
-    if ( tambahan && tambahan.isActive )
-    {
-      result.tambahan = tambahan
-    }
-    if ( discount && discount.isActive )
-    {
-      result.discount = discount
-    }
 
-    return res.status( 200 ).json( {
+    return res.status(200).json({
       message: 'Ok',
       data: result
-    } )
-  } catch ( error )
-  {
-    return res.status( 500 ).json( {
+    })
+  } catch (error) {
+    return res.status(500).json({
       message: error.message
-    } )
+    })
   }
 }
 
